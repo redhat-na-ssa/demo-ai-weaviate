02ce408f3af133bcaf412ff38566c8f82c26acb5
Bob Kozdemba <bkozdemba@gmail.com>
2024-02-08 10:38:46 -0800

    fixed import block

--- src/01-create-schema-import-data.py
+++ src/01-create-schema-import-data.py
@@ -34,23 +34,23 @@ else:
     client.schema.create_class(class_obj)
 
 
-#
-# Load some questions.
-#
-resp = requests.get(
-    'https://raw.githubusercontent.com/weaviate-tutorials/quickstart/main/data/jeopardy_tiny.json')
-data = json.loads(resp.text)  # Load data
+    #
+    # Load some questions.
+    #
+    resp = requests.get(
+        'https://raw.githubusercontent.com/weaviate-tutorials/quickstart/main/data/jeopardy_tiny.json')
+    data = json.loads(resp.text)  # Load data
 
-client.batch.configure(batch_size=100)  # Configure batch
-with client.batch as batch:  # Initialize a batch process
-    for i, d in enumerate(data):  # Batch import data
-        print(f"importing question: {i+1}")
-        properties = {
-            "answer": d["Answer"],
-            "question": d["Question"],
-            "category": d["Category"],
-        }
-        batch.add_data_object(
-            data_object=properties,
-            class_name="Question"
-        )
+    client.batch.configure(batch_size=100)  # Configure batch
+    with client.batch as batch:  # Initialize a batch process
+        for i, d in enumerate(data):  # Batch import data
+            print(f"importing question: {i+1}")
+            properties = {
+                "answer": d["Answer"],
+                "question": d["Question"],
+                "category": d["Category"],
+            }
+            batch.add_data_object(
+                data_object=properties,
+                class_name="Question"
+            )
